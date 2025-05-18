6a6fff926438434e7b579baec56669bf84528d2c
Niabari NI <155182761+Niabari-NI-2@users.noreply.github.com>
2025-05-18 23:22:16 +0100

    Just slight improvements

--- src/pages/OrientationSub/OrientationVids.tsx
+++ src/pages/OrientationSub/OrientationVids.tsx
@@ -21,7 +21,7 @@ export default function OrientationVids() {
           <p className='text-3xl font-serif text-black py-3'>Videos</p>
           
           <div className='mb-5 grid grid-cols-3 gap-2'>
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/FLpn1u333U8?si=i0jHfIyQ87IIbqB0" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -32,7 +32,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/y-S9-U8n9eY?si=9rmBxs97hRsUbloE" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -43,7 +43,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/aZVKGiBWDSI?si=SwJ0FFknHTQvQjIl" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -54,7 +54,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/MmJc-8M4HG4?si=KBPihXWokD3pjUWg" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -65,7 +65,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/FcGcgPuP844?si=-YhwLg3GLyCnVNi2" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -76,7 +76,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/WDTaFYQugf0?si=t6WW8nvFIeAPKaT6" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -87,7 +87,7 @@ export default function OrientationVids() {
               </div>
             </div>
 
-            <div className='container w-max p-4 bg-violet-200 rounded-md mb-2'>
+            <div className='container w-max p-4 bg-violet-200 rounded-md shadow-md mb-2'>
               <iframe className='p-4' src="https://www.youtube.com/embed/mWo1apygv6o?si=EHpQnTqbdCrLBuP3" 
               title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
